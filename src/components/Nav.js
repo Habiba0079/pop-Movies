27f5c3ca035095de0f4f020bf2496669ce43e47c
Habiba Hmed <habibamohamed3880@gmail.com>
2025-08-25 21:38:28 +0300

    add search components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,16 +5,21 @@ export default function Nav({ query, setQuery, movies }) {
         <span role="img">🍿</span>
         <h1>usePopcorn</h1>
       </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <Search setQuery={setQuery} query={query} />
       <p className="num-results">
         Found <strong>{movies.length}</strong> results
       </p>
     </nav>
+  );
+}
+function Search({ setQuery, query }) {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }

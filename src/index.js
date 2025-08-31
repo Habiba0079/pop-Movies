8f44f242c6208def4102c5a406d39b352201f5f2
Habiba Hmed <habibamohamed3880@gmail.com>
2025-08-31 13:18:38 +0300

    adding starRating component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from "./components/StarRating"
+
+
+function Test(){
+  const [rating, setRating] = React.useState(0);
+  return (
+    <>
+      <StarRating maxRating={10} onRate={setRating} />
+      <p>this movie is rated {rating} </p>
+    </>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating size={24} color="#ff0000" />
+    <Test />
   </React.StrictMode>
 );
 

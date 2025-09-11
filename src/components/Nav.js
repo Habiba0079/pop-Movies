1a817804b14fd3e75104e6f04943087ddefaf65d
Habiba Hmed <habibamohamed3880@gmail.com>
2025-09-11 17:03:32 +0300

    final styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,8 @@ export function Nav({ children }) {
 export function Logo() {
   return (
     <div className="logo">
-      <span role="img">🍿</span>
-      <h1>usePopcorn</h1>
+      <span role="img">🎬</span>
+      <h1>PopMovies</h1>
     </div>
   );
 }
